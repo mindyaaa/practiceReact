ecaa77d322c0863520d195161277980bc92aa123
mindyaaa <107449479+mindyaaa@users.noreply.github.com>
2023-02-03 12:15:10 +0900

    wrap practice

---
+++ src/components/AppWrap.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+export default function AppWrap() {
+    return (
+        <div>
+            <Navbar>
+                <Avatar
+                image='https://images.unsplash.com/photo-1554151228-14d9def656e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80'
+                name='linda'
+                size={300}
+                />
+            </Navbar>
+        </div>
+    );
+}
+
+
+function Navbar ({children}) {
+    return (
+        <header style={{backgroundColor:'yellow'}}>
+            {children}
+        </header>
+    )
+}
+
+function Avatar ({image, name, size}) {
+    return (
+        <>
+         <img
+            src={image}
+            alt={name}
+            width={size}
+            height={size}
+            style={{borderRadius:'50%'}}
+          />
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AppForm from './components/AppForm';
+import AppWrap from './components/AppWrap';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppForm />
+    <AppWrap />
   </React.StrictMode>
 );
 
