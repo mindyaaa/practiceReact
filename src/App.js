db28adc9d09d7dfeb6152ba2eae4537be7239bd9
mindyaaa <107449479+mindyaaa@users.noreply.github.com>
2023-02-08 15:08:32 +0900

    useParam 적용

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Videos from './pages/Videos';
 import Root from './pages/Root';
+import VideoDetail from './pages/VideoDetail';
 
 const router = createBrowserRouter([
   {
@@ -15,7 +16,8 @@ const router = createBrowserRouter([
     errorElement: <NotFound />,
     children : [
       { index: true, element: <Home />},
-      { path : "/videos", element: <Videos/>}
+      { path : "/videos", element: <Videos/>},
+      { path : "/videos/:videoId", element : <VideoDetail />}
     ]
   }
 ])
